afd19b000d32472c27f37754ed1d8951d498788d
sutter@intransix.com <sutter@intransix.com@eb42b758-2485-dcde-ce72-a774ca66f41b>
2012-10-12 17:50:52 +0000

    some editting to get icons to work.

--- OpenLayersClient/web/indoormap/stylemap.js
+++ OpenLayersClient/web/indoormap/stylemap.js
@@ -54,80 +54,141 @@ function getStyleMap() {
 	
 	var styles = new OpenLayers.StyleMap(defaultStyle);
 	styles.addUniqueValueRules("default", "buildingpart", {
+		"wall": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 3, 
+			"fillColor":"#415a81"
+		},
+		"door": {
+			"strokeColor": "#55ac5a", 
+			"strokeWidth": 3, 
+			"fillColor":"#55ac5a"
+		},
 		"unit": {
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
+			"strokeWidth": 3, 
 			"fillColor":"#8ca9df"
 		},
 		"corridor": {
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
+			"strokeWidth": 0, 
 			"fillColor":"#d2dbe9"
 		},
 		"hall":{
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
+			"strokeWidth": 0, 
 			"fillColor":"#8ca9df"
 		},
 		"room": {
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
-			"fillColor":"#8ca9df"			
+			"strokeWidth": 0, 
+			"fillColor":"#8ca9df",
+			"fillOpacity":.3
 		},
-		"bathroom": {
+		"room|bathroom:yes": {
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
-			"fillColor":"#55ac5a",
-			"externalGraphic":"res/bathroom.png"
+			"strokeWidth": 0, 
+			"fillColor":"#73bec5",
+			"externalGraphic":"res/bathroom.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"room|bathroom:male": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 0, 
+			"fillColor":"#73bec5",
+			"externalGraphic":"res/bathroom_male.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"room|bathroom:female": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 0, 
+			"fillColor":"#73bec5",
+			"externalGraphic":"res/bathroom_female.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"room|bathroom:unisex": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 0, 
+			"fillColor":"#73bec5",
+			"externalGraphic":"res/bathroom.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
 		},
 		"kiosk": {
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
+			"strokeWidth": 3, 
 			"fillColor":"#798fb8"
 		},
 		"stairs":{
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
+			"strokeWidth": 1, 
 			"fillColor":"#b4c3dc",
 			"externalGraphic":"res/stairs.png",
-			"graphicOpacity":.8			
-			
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
 		},
 		"escalator": {
-			"externalGraphic":"res/escalator.png"
+			"strokeColor": "#415a81", 
+			"strokeWidth": 1, 
+			"fillColor":"#b4c3dc",
+			"externalGraphic":"res/escalator.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"escalator|direction:both": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 1, 
+			"fillColor":"#b4c3dc",
+			"externalGraphic":"res/escalator.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"escalator|direction:up": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 1, 
+			"fillColor":"#b4c3dc",
+			"externalGraphic":"res/escalator_up.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"escalator|direction:down": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 1, 
+			"fillColor":"#b4c3dc",
+			"externalGraphic":"res/escalator_down.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
+		},
+		"escalator|direction:arriving": {
+			"strokeColor": "#415a81", 
+			"strokeWidth": 1, 
+			"fillColor":"#b4c3dc",
+			"externalGraphic":"res/escalator.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
 		},
 		"elevator":{
 			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
+			"strokeWidth": 1, 
 			"fillColor":"#b4c3dc",
-			"externalGraphic":"res/elevator.png"	
+			"externalGraphic":"res/elevator.png",
+			"graphicHeight":25,
+			"graphicWidth":25,
+			"fillOpacity":.3
 		}		
 	});
-	styles.addUniqueValueRules("default", "buildingpart:verticalpassage", {
-		"stairway":{
-			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
-			"fillColor":"#b4c3dc",
-			"externalGraphic":"res/stairs.png"			
-		},
-		"elevator":{
-			"strokeColor": "#415a81", 
-			"strokeWidth": 2, 
-			"fillColor":"#b4c3dc",
-			"externalGraphic":"res/elevator.png"	
-		},
-		"escalator": {
-			"externalGraphic":"res/escalator.png"
-		}
-	});
-	styles.addUniqueValueRules("default", "shell", {
-		"yes": {
-			"strokeColor": "#000000", 
-			"strokeWidth": 2, 
-			"fillColor":"#eeeeee"
-		}		
-				
-	});
-
 	return styles;
 };
